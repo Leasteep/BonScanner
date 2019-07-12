9933a7019cc001ac6313bcfca31ab838f71e35c4
Marian Wiskow <4wiskow@informatik.uni-hamburg.de>
2019-07-12 15:41:05 +0200

    Initial Commit. Snapshot Scene created. Camera access script added.

---
+++ Assets/PhoneCamera.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneCamera : MonoBehaviour
+{
+    private bool camAvailable;
+    private WebCamTexture backCam;
+    private Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    private void Start()
+    {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length < 1)
+        {
+            Debug.Log("No Camera available");
+            camAvailable = false;
+            return;
+        }
+
+        // only uses last found back camera...
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (devices[i].isFrontFacing)
+            {
+                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+
+        if(backCam == null)
+        {
+            Debug.Log("No front facing camera found");
+            return;
+        }
+
+        backCam.Play();
+        background.texture = backCam;
+        camAvailable = true;
+
+    }
+
+    private void Update()
+    {
+        if (!camAvailable)
+            return;
+
+        float ratio = (float)backCam.width / (float)backCam.height;
+        fit.aspectRatio = ratio;
+
+        // landscape vs. portrait mode
+        float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -backCam.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+    }
+
+}
